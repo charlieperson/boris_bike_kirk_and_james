a97ec0fb10edec4fc73fa21eada3dbb6236f4cc0
Charlie Person <charlie.person@hyperisland.se>
2015-11-26 12:25:11 +0000

    CH16 RSpec passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,22 +1,36 @@
 require_relative 'bike'
 
 class DockingStation
+	DEFAULT_CAPACITY = 20
+
 	attr_reader :bikes
+
 	def initialize
 		@bikes = []
 	end
 
 	def release_bike
-		fail 'No bikes available!' unless @bike
-		@bike
+		fail 'No bikes available!' if empty?
+		@bikes.pop
 	end
 
 	def dock(bike)
-		fail 'Docking station full!' if @bikes.length >= 20
+		fail 'Docking station full!' if full?
 		@bikes << bike
 	end
+
 	def count
 		@bikes.length
+	end
+
+	private
+
+	def full?
+		@bikes.length >= DEFAULT_CAPACITY
+	end
+
+	def empty?
+		@bikes.empty?
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,20 +17,19 @@ describe DockingStation do
 
 	describe '#dock' do
 		it 'raises an error when full' do
-			subject.dock(Bike.new)
+			DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
 			expect {subject.dock Bike.new}.to raise_error 'Docking station full!'
 		end
 	end
 
 	it 'is bike docked' do
 		bike = Bike.new
-		subject.dock(bike)
-		expect(subject.bike).to eq bike
+		expect(subject.dock(bike)).to eq subject.bikes
 	end
 
 	it 'docking station full' do
-		20.times{subject.dock Bike.new}
-		expect(subject.count).to eq 20
+		DockingStation::DEFAULT_CAPACITY.times{subject.dock Bike.new}
+		expect(subject.count).to eq DockingStation::DEFAULT_CAPACITY
 	end
 
 
